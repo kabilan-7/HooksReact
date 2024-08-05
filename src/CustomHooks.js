fd68dd939cef28c68a516fba4a56fea9e8822d5f
kabilan-7 <murugankabilan39@gmail.com>
2024-08-05 22:52:38 +0530

    changes 2

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import useLocalStorage from './useLocalStorage'
+import useUpdateLogger from './useUpdateLogger'
 
 const CustomHooks = () => {
     const [data,setData]=useLocalStorage('name','')
+    useUpdateLogger(data)
   return (
     <div>
         <input value={data} type="text" onChange={(e)=>setData(e.target.value)} />
